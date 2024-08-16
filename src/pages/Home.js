874a811196ac0a194341ad02cf854bf336b38ce2
Amirtaraj Ragavan <amragavan@BLR.ALLEGISINDIA.COM>
2024-08-16 23:22:50 +0530

    v1.3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,48 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import './IPhoneGuide.css'; // Ensure this file is in the correct path
+import buyersImage from './images/buyers-guide.jpg'; // Update with your image name
+import iPhoneImage from './images/iphone-guide.jpg';
+import iPadImage from './images/ipad-guide.jpg';
+import watchImage from './images/watch-guide.jpg';
+import airpodsImage from './images/airpods-guide.jpg';
+import macImage from './images/mac-guide.jpg';
+import homeImage from './images/home-guide.jpg';
+import visionImage from './images/vision-guide.jpg';
+import accessoriesImage from './images/accessories-guide.jpg';
 
-import './IPhoneGuide.css'; // Ensure to add this line if you create a CSS file for additional styling
-import iPhoneImage from './images/buyers-guide.jpg'; // Update with your image name
-
-function Home() {
+function IPhoneGuide() {
   return (
     <div className="image-container">
-      <img src={iPhoneImage} alt="iPhone Guide" className="responsive-image" />
+      <Link to="/">
+        <img src={buyersImage} alt="Apple Guide" className="responsive-image" />
+      </Link>
+      <Link to="/iphone">
+        <img src={iPhoneImage} alt="iPhone Guide" className="responsive-image" />
+      </Link>
+      <Link to="/ipad">
+        <img src={iPadImage} alt="iPad Guide" className="responsive-image" />
+      </Link>
+      <Link to="/watch">
+        <img src={watchImage} alt="Watch Guide" className="responsive-image" />
+      </Link>
+      <Link to="/airpods">
+        <img src={airpodsImage} alt="AirPods Guide" className="responsive-image" />
+      </Link>
+      <Link to="/mac">
+        <img src={macImage} alt="Mac Guide" className="responsive-image" />
+      </Link>
+      <Link to="/home">
+        <img src={homeImage} alt="Home Guide" className="responsive-image" />
+      </Link>
+      <Link to="/vision">
+        <img src={visionImage} alt="Vision Guide" className="responsive-image" />
+      </Link>
+      <Link to="/accessories">
+        <img src={accessoriesImage} alt="Accessories Guide" className="responsive-image" />
+      </Link>
     </div>
   );
 }
 
-export default Home;
+export default IPhoneGuide;
